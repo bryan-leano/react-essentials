f1a6aa4a58ddb6ad622cd62dedf7e8eee9d7e2c1
Bryan Leanoe <bryan.leano@gmail.com>
2021-08-10 15:41:14 -0600

    add restructuring

--- src/index.js
+++ src/index.js
@@ -3,7 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+const [, , light] = ["boots", "tent", "headlamp"];
+console.log(light);
+
 ReactDOM.render(
-  <App />,
+  <App authorized={false} />,
   document.getElementById('root')
 );
